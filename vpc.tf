1d6e0e23d6db507261b680bad22be2cded1c5e8b
Victor <yeforriak@gmail.com>
2017-04-27 15:03:10 +0100

    Create subnet and associate it with the route table that was associated with the VPC

--- vpc.tf
+++ vpc.tf
@@ -12,15 +12,32 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_route_table" "main" {
-    vpc_id = "${aws_vpc.main.id}"
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.main.id}"
-    }
-
-    tags {
-        Name = "victorf-leap-forward-public-subnet"
-    }
+resource "aws_subnet" "public-subnet" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "${var.public_subnet_cidr}"
+  tags {
+    Name = "victorf-leap-forward-public-subnet"
+  }
 }
+
+#Defining route table for the VPC, it needs to get associated to the public subnet
+resource "aws_route_table" "all-from-internet-gateway" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+
+  tags {
+    Name = "victorf-leap-forward-public-subnet"
+  }
+}
+
+resource "aws_route_table_association" "public-subnet-to-internet-gateway" {
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.all-from-internet-gateway.id}"
+}
+
+
+
